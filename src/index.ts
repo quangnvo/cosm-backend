d97223125c1b07243172281cf9ed184f23c5f8fa
quangnvo <voquang.usth@gmail.com>
2023-11-20 23:23:52 +0800

    create simle routing

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-const name: string = "Dư Thanh Được";
+import express from "express";
 
-console.log(name);
+const app = express();
+const router = express.Router();
+
+const port = 3000;
+
+app.get("/", (req: any, res: any) => {
+  res.send("Hello World!");
+});
+
+router.get("/tweet", (req: any, res: any) => {
+  res.send("Hello aaaaaa!");
+});
+
+app.use("/api", router);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
