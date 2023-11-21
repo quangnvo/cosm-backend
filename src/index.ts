35db84074ce786833e3f722e5c919e7e596c5aac
quangnvo <voquang.usth@gmail.com>
2023-11-21 21:42:10 +0800

    add the userRouter

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,14 @@
 import express from "express";
+import userRouter from "./user.routes";
 
 const app = express();
-const router = express.Router();
-
 const port = 3000;
 
-app.get("/", (req: any, res: any) => {
+app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-router.get("/tweet", (req: any, res: any) => {
-  res.send("Hello aaaaaa!");
-});
-
-app.use("/api", router);
+app.use("/api", userRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
