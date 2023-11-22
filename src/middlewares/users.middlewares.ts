98a99eee5d6f0e8cf98fe3f21a137ca00bc7f2d4
quangnvo <voquang.usth@gmail.com>
2023-11-22 22:00:16 +0800

    add the registerValidator

--- src/middlewares/users.middlewares.ts
+++ src/middlewares/users.middlewares.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from "express";
+import { checkSchema } from "express-validator";
 
 export const loginValidator = (
   req: Request,
@@ -10,6 +11,70 @@ export const loginValidator = (
     return res.status(400).json({ message: "Email and password are required" });
   }
   next();
-  next();
-  next();
 };
+
+export const registerValidator = checkSchema({
+  name: {
+    notEmpty: true,
+    isString: true,
+    isLength: {
+      options: {
+        min: 1,
+        max: 255,
+      },
+    },
+    trim: true,
+  },
+  email: {
+    notEmpty: true,
+    isEmail: true,
+    normalizeEmail: true,
+    trim: true,
+  },
+  password: {
+    notEmpty: true,
+    isString: true,
+    isLength: {
+      options: {
+        min: 6,
+        max: 50,
+      },
+    },
+    isStrongPassword: {
+      options: {
+        minLength: 6,
+        minSymbols: 1,
+        minNumbers: 1,
+        minUppercase: 1,
+        minLowercase: 1,
+      },
+    },
+  },
+  password_confirm: {
+    notEmpty: true,
+    isString: true,
+    isLength: {
+      options: {
+        min: 6,
+        max: 50,
+      },
+    },
+    isStrongPassword: {
+      options: {
+        minLength: 6,
+        minSymbols: 1,
+        minNumbers: 1,
+        minUppercase: 1,
+        minLowercase: 1,
+      },
+    },
+  },
+  date_of_birth: {
+    isISO8601: {
+      options: {
+        strict: true,
+        strictSeparator: true,
+      },
+    },
+  },
+});
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -4,10 +4,25 @@ import {
   loginController,
   registerController,
 } from "~/controllers/users.controllers";
+import { registerValidator } from "~/middlewares/users.middlewares";
 
 const usersRouter = Router();
 
 usersRouter.post("/login", loginValidator, loginController);
-usersRouter.post("/register", registerController);
+
+/* 
+Description: Register a new user
+Route: /users/register
+Methods: POST
+Body: {
+  "name": "string",
+  "email": "string",
+  "password": "string",
+  "password_confirm": "string",
+  "date_of_birth": "string",
+}
+*/
+
+usersRouter.post("/register", registerValidator, registerController);
 
 export default usersRouter;
