d999c0e25abca3c975fc06d6c58e818b1348dd49
quangnvo <voquang.usth@gmail.com>
2023-11-23 10:09:50 +0800

    add the hashpassword function to crypto the password

--- src/models/schemas/User.schema.ts
+++ src/models/schemas/User.schema.ts
@@ -1,10 +1,5 @@
 import { ObjectId } from "mongodb";
-
-enum UserVerifyStatus {
-  Unverified,
-  Verified,
-  Banned,
-}
+import { UserVerifyStatus } from "~/constants/enums";
 
 interface UserType {
   _id?: ObjectId;
--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -1,18 +1,22 @@
 import { User } from "~/models/schemas/User.schema";
 import databaseService from "./database.services";
 import { RegisterReqBodyType } from "~/models/requests/User.requests";
+import { hashPassword } from "~/utils/crypto";
 
 class UsersService {
+  // Register
   register = async (payload: RegisterReqBodyType) => {
     const result = await databaseService.users.insertOne(
       new User({
         ...payload,
         date_of_birth: new Date(payload.date_of_birth),
+        password: hashPassword(payload.password),
       })
     );
     return result;
   };
 
+  // Check email exists
   checkEmailExists = async (email: string) => {
     const user = await databaseService.users.findOne({ email });
     return Boolean(user);
